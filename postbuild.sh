efc451f1fca72d48ee2685d722d7a2117e18871a
mujdecisy <mujdecisy@gmail.com>
2023-08-25 16:45:40 +0300

    fix: add build script

--- postbuild.sh
+++ postbuild.sh
@@ -1,2 +1,2 @@
-sed -i -e 's/start_url": "\//start_url": "\/pwa-check/g' web-build/manifest.json
+sed -i -e 's/start_url": "\//start_url": "\/pwa-memorise/g' web-build/manifest.json
 sed -i -e 's/ffffff/ffd700/g' web-build/manifest.json
